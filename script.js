20f77a9fe15bb05a38e3873358e5017308e57647
CodinghakdangDaniel <junh2e97@gmail.com>
2023-09-16 01:39:41 +0900

    sideproject

---
+++ script.js
@@ -0,0 +1,13 @@
+function setFull() {
+    document.getElementById('bar1').value = '100';
+};
+
+function setZero() {
+    document.getElementById('bar1').value = '0';
+};
+
+/* iframe 설정 */
+function ifun(msg) {
+    alert(msg);
+};
+
