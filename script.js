14514b37c6af59ad7f3735c01c48ba235738faf6
CodinghakdangDaniel <junh2e97@gmail.com>
2023-09-17 23:58:37 +0900

    lab17

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ function ifun(msg) {
     alert(msg);
 };
 
+// $(document).ready(function(){  
+// 	$('#resetbutton').click(function(){  
+// 		$('#form1 input').each(function(){
+// 			if($(this).hasClass('reset1')) $(this).val('');  
+// 		});  
+// 	});  
+//  });  
